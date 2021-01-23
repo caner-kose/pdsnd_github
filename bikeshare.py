5c32b6dcf4598e568afc4cb720800e0cbc00be94
canerkose <canerkose@outlook.de>
2021-01-23 09:05:29 +0300

    refactor: Add comments line in doc

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,10 @@ import time
 import pandas as pd
 import numpy as np
 
+#TEST for github udacity
+
+#Test Bikeshare.py
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
